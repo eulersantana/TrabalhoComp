ec9544ef80dc8fb53a3d01192f44b9dbd644184f
Euler Santana <euler.santana@yahoo.com>
2014-09-15 16:45:16 -0300

    up de funcoes

--- funcoes.c
+++ funcoes.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "estrutura.h"
+
+int addErro(int linha, char *erro){
+
+}
+
+void leituraArquivo(FILE *arquivo, char* modo){
+
+}
+
+int analiseLexica(char* texto){
+
+}
